b52a13d105aada42287a740d0284317efab2dee2
PrudentialDevelolper <dev.prudential@gmail.com>
2023-01-24 08:04:06 +0530

    Get Latest Products

--- includes/home/section-two.php
+++ includes/home/section-two.php
@@ -1,52 +1,35 @@
 <div class="row mt-3">
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="img/1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div class="text-center"><a href="#" class="btn btn-primary">Go somewhere</a></div>
-                    </div>
-                </div>
-            </div>
+
+
+    <?php
+    $getLatestP = "SELECT * FROM product ORDER BY p_id DESC LIMIT 4";
+    $getLatestP = $conn->prepare($getLatestP);
+    $getLatestP->execute();
+    $latestrows = $getLatestP->rowCount();
+    if ($latestrows > 0) {
+        while ($fetchLatestP = $getLatestP->fetch()) {
+
+    ?>
 
             <div class="col-md-3">
                 <div class="card">
                     <img src="img/1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div class="text-center"><a href="#" class="btn btn-primary">Go somewhere</a></div>
-
+                        <h5 class="card-title"><?php echo $fetchLatestP['p_name'] ?></h5>
+                        <p class="card-text mb-0">Rs. <?php echo $fetchLatestP['p_price'] ?></p>
+                        <p class="card-text">Available Qty : <?php echo $fetchLatestP['p_qty'] ?></p>
+                        <div class="text-center"><a href="add_to_cart.php?" class="btn btn-primary">Add To Cart</a></div>
                     </div>
                 </div>
             </div>
 
 
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="img/1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div class="text-center"><a href="#" class="btn btn-primary">Go somewhere</a></div>
-
-                    </div>
-                </div>
-            </div>
+    <?php
 
 
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="img/1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div class="text-center"><a href="#" class="btn btn-primary">Go somewhere</a></div>
+        }
+    }
 
-                    </div>
-                </div>
-            </div>
+    ?>
 
-
-        </div>
+</div>
